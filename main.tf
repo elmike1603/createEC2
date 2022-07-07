b84e926a3aca8a36bc1c43c6d82891cc2c19f88d
Mike <elmike1603@gmail.com>
2022-07-06 23:30:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "aws_key_pair" "deployer1" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDD8gTEP0wdqJnHkVDc7IMzFygpsibTemZylZvk4gcGiaa+YX8/VpUEyOrqYOg/8OrOAsdQCpPcc8J3AhUBSvgjeKc7sJZ0N8v+AOKnVQDni/y+9mWD6oGOszzH6DVfoHOy1HRZAbF6n+xFkk0DeisSJ3FsGVQK/J5rYgXVCBJii34mL0+DOBogxLva8tDcLgWLz+qxs8QcH4hgrHkdT8whBKfKcypICGS4U/WgXFW5M3pN4OynSzeqJ9gXn0Fbq50J/t6Cpnr6wqlzRzocAMXnlgrchYZWZJgTL3W8zrg1Fx7RBy3fAbEyWoyrkB9cFIPSkmiUZG9xUhirI4MbdgHVNlkp7xU0V6KfCRlTNE+xzWlbPSga7kjlqiizjDoR7xExKNLbNthB6nlNIEwX+FljtgrTqMV2w6k8H3xfrhdG0993EnMMwdPVmltEjJeZ+l3BETUcibKGFFc5iyR99W7NKhriH5d6OvbXev7JmbqsXrwn8rnu3auDGJs7YaIwzmk= varunmanikoutlo@ip-172-31-17-206"
 }
 
-resource "aws_instance" "linux" {
+resource "aws_instance" "ansible" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
   key_name      = aws_key_pair.deployer1.key_name
@@ -67,7 +67,7 @@ resource "aws_instance" "linux" {
 ####### Ubuntu VM #####
 
 
-resource "aws_instance" "ubuntu" {
+resource "aws_instance" "webserver" {
   ami                    = "ami-04505e74c0741db8d"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.deployer1.key_name
