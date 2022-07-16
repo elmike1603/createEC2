5e2d00bd08d0bc54b824ca9cff26290e9580a4c1
Mike <elmike1603@gmail.com>
2022-07-16 16:22:45 -0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -27,12 +27,6 @@ resource "aws_security_group" "allow_SSH" {
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-    # description      = "SSH from VPC"
-    # from_port        = 22
-    # to_port          = 22
-    # protocol         = "tcp"
-    # cidr_blocks      = ["61.6.14.46/32"]
-    # # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
 
   egress {
@@ -49,22 +43,15 @@ resource "aws_key_pair" "deployer1" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDD8gTEP0wdqJnHkVDc7IMzFygpsibTemZylZvk4gcGiaa+YX8/VpUEyOrqYOg/8OrOAsdQCpPcc8J3AhUBSvgjeKc7sJZ0N8v+AOKnVQDni/y+9mWD6oGOszzH6DVfoHOy1HRZAbF6n+xFkk0DeisSJ3FsGVQK/J5rYgXVCBJii34mL0+DOBogxLva8tDcLgWLz+qxs8QcH4hgrHkdT8whBKfKcypICGS4U/WgXFW5M3pN4OynSzeqJ9gXn0Fbq50J/t6Cpnr6wqlzRzocAMXnlgrchYZWZJgTL3W8zrg1Fx7RBy3fAbEyWoyrkB9cFIPSkmiUZG9xUhirI4MbdgHVNlkp7xU0V6KfCRlTNE+xzWlbPSga7kjlqiizjDoR7xExKNLbNthB6nlNIEwX+FljtgrTqMV2w6k8H3xfrhdG0993EnMMwdPVmltEjJeZ+l3BETUcibKGFFc5iyR99W7NKhriH5d6OvbXev7JmbqsXrwn8rnu3auDGJs7YaIwzmk= varunmanikoutlo@ip-172-31-17-206"
 }
 
-resource "aws_instance" "ansible" {
+resource "aws_instance" "Tomcat-Server" {
   ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
   key_name      = aws_key_pair.deployer1.key_name
   # count         = 1 
   vpc_security_group_ids = ["${aws_security_group.allow_SSH.id}"]
   tags = {
-    "Name" = "ansible"
+    "Name" = "Tomcat-Server"
     "ENV"  = "Dev"
-  }
-  # Type of connection to be established
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = file("./deployer")
-    host        = self.public_ip
   }
 
   depends_on = [aws_key_pair.deployer1]
@@ -74,13 +61,14 @@ resource "aws_instance" "ansible" {
 ####### Ubuntu VM #####
 
 
-resource "aws_instance" "webserver" {
+resource "aws_instance" "Jenkin-Server" {
   ami                    = "ami-04505e74c0741db8d"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.deployer1.key_name
   vpc_security_group_ids = ["${aws_security_group.allow_SSH.id}"]
+  user_data       	 = "${file("install_jenkins.sh")}"
   tags = {
-    "Name" = "webserver"
+    "Name" = "Jenkin-Server"
     "ENV"  = "Dev"
   }
   # Type of connection to be established
@@ -90,7 +78,6 @@ resource "aws_instance" "webserver" {
     private_key = file("./deployer")
     host        = self.public_ip
   }
- 
 
   depends_on = [aws_key_pair.deployer1]
 
