0b16b15440024df479ac0546f26e9d8a8b756c7e
Mike <elmike1603@gmail.com>
2022-07-16 18:56:30 -0500

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
-sudo apt-get update -y
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo apt update && upgrade
+sudo apt install -y python3.8
+sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
-sudo apt -y install openjdk-8-jdk
-sudo apt -y install jenkins git
+sudo apt-get install -y openjdk-8-jre
+sudo apt-get install -y jenkins
 sudo systemctl daemon-reload
 sudo systemctl start jenkins
-sudo systemctl status jenkin
+sudo systemctl status jenkins
