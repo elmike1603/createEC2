a94763a18b106054848e3a41fcfcc9226e217bc1
Mike <elmike1603@gmail.com>
2022-07-17 13:57:51 -0500

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -14,4 +14,4 @@ sudo systemctl status jenkins
 sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install -y ansible
